420aca2fed71ccfc3807c47dd0322427e990e93a
thanos karakos <103602897+thanosKarakos17@users.noreply.github.com>
2023-11-08 22:28:43 +0200

    Add files via upload

--- logger.c
+++ logger.c
@@ -1,18 +1,57 @@
 #define _GNU_SOURCE
 
+#include <time.h>
 #include <stdio.h>
 #include <dlfcn.h>
-#include <unistd.h> // Include unistd.h for access
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <openssl/md5.h>
 
-FILE* fopen(const char* path, const char* mode){
-	if (access(path, F_OK) != -1) {
-        // The file exists
-        printf("File already exists: %s\n", path);
-    } else {
-        // The file does not exist
-        printf("File does not exist, new file created: %s\n", path);
-    }
-	FILE* (original_fopen)(const char*, const char*);
+FILE *
+fopen(const char *path, const char *mode) 
+{
+
+	FILE *original_fopen_ret;
+	FILE *(*original_fopen)(const char*, const char*);
+
+	/* call the original fopen function */
 	original_fopen = dlsym(RTLD_NEXT, "fopen");
-	return (*original_fopen)(path, mode);
+	original_fopen_ret = (*original_fopen)(path, mode);
+
+
+	/* add your code here */
+	/* ... */
+	/* ... */
+	/* ... */
+	/* ... */
+
+
+	return original_fopen_ret;
 }
+
+
+size_t 
+fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream) 
+{
+
+	size_t original_fwrite_ret;
+	size_t (*original_fwrite)(const void*, size_t, size_t, FILE*);
+
+	/* call the original fwrite function */
+	original_fwrite = dlsym(RTLD_NEXT, "fwrite");
+	original_fwrite_ret = (*original_fwrite)(ptr, size, nmemb, stream);
+
+
+	/* add your code here */
+	/* ... */
+	/* ... */
+	/* ... */
+	/* ... */
+
+
+	return original_fwrite_ret;
+}
+
+
