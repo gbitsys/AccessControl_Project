1dd55f6574d4e0323bd2ed4e8eafce184cb08340
thanos karakos <103602897+thanosKarakos17@users.noreply.github.com>
2023-11-13 22:27:54 +0200

    Update acmonitor.c

--- acmonitor.c
+++ acmonitor.c
@@ -4,28 +4,27 @@
 #include <string.h>
 #include <unistd.h>
 
-struct entry {
-
+typedef struct log_entry {
 	int uid; /* user id (positive integer) */
 	int access_type; /* access type values [0-2] */
 	int action_denied; /* is action denied values [0-1] */
 
-	time_t date; /* file access date */
-	time_t time; /* file access time */
+	char* date; /* file access date */
+	char* time; /* file access time */
 
-	char *file; /* filename (string) */
 	char *fingerprint; /* file fingerprint */
+	char* filename;
 
-	/* add here other fields if necessary */
-	/* ... */
-	/* ... */
-
-};
+}Entry;
 
 
 void
 usage(void)
-{
+{	
+	Entry* new = malloc(sizeof(Entry));
+	get_log_entry(new, fopen("./file_logging.log", "r"));
+	printf("uid %d\nfile %s\ndate %s\ntime %s\nkid named finger %s\n", new->uid, new->filename, new->date, new->time, new->fingerprint);
+	
 	printf(
 	       "\n"
 	       "usage:\n"
@@ -36,36 +35,137 @@ usage(void)
 		   "the file <filename> and the number of modifications\n"
 		   "-h, Help message\n\n"
 		   );
-
 	exit(1);
+}
+
+void get_log_entry(Entry* log_entry, FILE* logfile){
+	log_entry->filename = malloc(sizeof(char)*50);
+	log_entry->fingerprint = malloc(sizeof(char)*50);
+	log_entry->date = malloc(sizeof(char)*20);
+	log_entry->time = malloc(sizeof(char)*20);
+	char day[4], mon[4], n[3], year[5];
+	//fscanf(logfile, "%d %s", &(log_entry->uid), log_entry->filename);
+
+	fscanf(logfile, "%d %s %s %s %s %s %s %d %d %s", &(log_entry->uid), log_entry->filename, day, mon, n, year, log_entry->time, &(log_entry->access_type), &(log_entry->action_denied), log_entry->fingerprint);
+	strcat(log_entry->date, day);
+	strcat(log_entry->date, " ");
+	strcat(log_entry->date, mon);
+	strcat(log_entry->date, " ");
+	strcat(log_entry->date, n);
+	strcat(log_entry->date, " ");
+	strcat(log_entry->date, year);
+	//fscanf(logfile, "%s %d %d %s", log_entry->time, &(log_entry->access_type), &(log_entry->action_denied), log_entry->fingerprint);
+}
+
+int check_user(int array[], int user){
+	int size = sizeof(array) / sizeof(user);
+	for(int i=0;i<size;i++){
+		if(array[i] == user)
+			return i;
+	}
+	return -1;	//not found
 }
 
 
 void 
 list_unauthorized_accesses(FILE *log)
 {
+	fseek(log, 0, SEEK_END); 
+	int size = ftell(log); 
+	fseek(log, 0, SEEK_SET); 
 
-	/* add your code here */
-	/* ... */
-	/* ... */
-	/* ... */
-	/* ... */
+	Entry** log_list = malloc(size*sizeof(Entry*));
 
+	int mal_users[size];
+	int denies[size]; 
+	memset(denies, 0, sizeof(denies));	//all elements set to 0
+	int index = 0;
+	
+	for(int i = 0;i<size;i++){
+		log_list[i] = malloc(sizeof(Entry));
+		get_log_entry(log_list[i], log);
+		if(log_list[i]->action_denied == 1){
+			int user_exists = check_user(mal_users, log_list[i]->uid);
+			if(user_exists != -1){	//user exists in mal_list
+				denies[user_exists]++;
+			}
+			else{
+				mal_users[index] = log_list[i]->uid;
+				denies[index]++;
+				index++;
+			}
+		}
+	}
+
+	for(int i = 0;i<index;i++){
+		if(denies[i] > 7){
+			printf("user id: %d\n", mal_users[i]);
+		}
+	}
+
+
+
+	for(int i = 0;i<size;i++){
+		free(log_list[i]);
+	}
+	free(log_list);
 	return;
-
 }
 
 
 void
 list_file_modifications(FILE *log, char *file_to_scan)
 {
+	fseek(log, 0, SEEK_END); 
+	int size = ftell(log); 
+	fseek(log, 0, SEEK_SET); 
 
-	/* add your code here */
-	/* ... */
-	/* ... */
-	/* ... */
-	/* ... */
+	Entry** log_list = malloc(size*sizeof(Entry*));
+	
+	int real_size = 0;
+	for(int i = 0;i<size;i++){
+		Entry* temp_log = malloc(sizeof(Entry));
+		get_log_entry(temp_log, log);
+		if(strcmp(realpath(file_to_scan, NULL), temp_log->filename) == 0){
+			log_list[real_size] = malloc(sizeof(Entry));
+			log_list[real_size] = temp_log;
+			real_size++;
+		}
+		//free(temp_log);
+	}
 
+	int users[real_size];
+	int mods[real_size]; 
+	memset(mods, 0, sizeof(mods));	//all elements set to 0
+	char* hash_value = log_list[0]->fingerprint;
+	users[0] = log_list[0]->uid;
+	mods[0]++;
+	int index = 1;
+	for(int i = 0;i<real_size;i++){
+		char* temp_value = log_list[i]->fingerprint;
+		if(strcmp(temp_value, hash_value) != 0){
+			int check = check_user(users, log_list[i]->uid);
+			if(check != -1){	//user exists in list
+				mods[check]++;
+			}
+			else{
+				users[index] = log_list[i]->uid;
+				mods[index]++;
+				index++;
+			}
+		hash_value = log_list[i]->fingerprint;
+		}
+	}
+
+	for(int i=0;i<index; i++){
+		printf("user id: %d modified the file %d times\n", users[i], mods[i]);
+	}
+
+
+	for(int i = 0;i<real_size;i++){
+		free(log_list[i]);
+	}
+	free(log_list);
 	return;
 
 }
@@ -100,13 +200,6 @@ main(int argc, char *argv[])
 		}
 
 	}
-
-
-	/* add your code here */
-	/* ... */
-	/* ... */
-	/* ... */
-	/* ... */
 
 
 	fclose(log);
